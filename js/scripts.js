f119e6790ddbc2dbbd0a3be15317e20c539b1263
Aurora Shido-Wagenet <fullmetalwoman@gmail.com>
2019-01-24 18:06:17 -0800

    halfway through input functionality, broken

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,33 @@
 function pigLatin(words){
   console.log(words);
+  var vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+  var currentWord;
+  var wordIndex;
+  var result;
+  var newWord;
+  var reorder = function(word){
+    console.log(words);
+    var wordIndexNumber = words.indexOf(wordIndex);
+    console.log(words.indexOf('f'));
+    console.log(wordIndexNumber);
+    newWord =  words.slice(wordIndexNumber);
+    console.log(newWord);
+  }
   words.forEach(function(character){
     var char = character.split("");
     console.log(char);
-    var result;
-    if (char.includes('a')){
-      result = true;
-    } else if (char.includes('e')){
-      result = true;
-    } else if (char.includes('i')){
-      result = true;
-    } else if (char.includes('o')){
-      result = true;
-    } else if (char.includes('u')){
-      result = true;
-    } else {
-      result = false;
-    }
-    console.log(result);
+    for (var i = 0; i < vowels.length; i++){
+      console.log(vowels[i]);
+        if (char.includes(vowels[i])){
+          wordIndex = vowels[i];
+          console.log(wordIndex);
+          result = true;
+          reorder(words);
+          console.log(newWord);
+      };
+    };
   });
-}
+};
 $(function(){
   $("form#pig-latin").submit(function(event){
     event.preventDefault();
